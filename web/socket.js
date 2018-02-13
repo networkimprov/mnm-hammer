a7455d6efdbe30f79c18a2abad985f5750614248
networkimprov <github@networkimprov.net>
2018-02-13 00:43:19 -0800

    web/socket: Add socket.js with API to mnm client backend

---
+++ web/socket.js
@@ -0,0 +1,140 @@
+// Copyright 2017 Liam Breck
+//
+// This file is part of the "mnm" software. Anyone may redistribute mnm and/or modify
+// it under the terms of the GNU Lesser General Public License version 3, as published
+// by the Free Software Foundation. See www.gnu.org/licenses/
+// Mnm is distributed WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See said License for details.
+
+;var mnm = {};
+(function() {
+   var sWs = null;
+
+   // caller implements these
+   mnm.Log = mnm.Render = mnm.ThreadChange = function(){};
+
+   mnm.SvcAdd = function(iObj) { // with name, addr, alias, loginperiod
+      sWs.send(JSON.stringify({op:'service_add', service:iObj}))
+   };
+
+   mnm.OhiAdd = function(iAliasTo) {
+      sWs.send(JSON.stringify({op:'ohi_add', ohi:{alias:iAliasTo}}))
+   };
+   mnm.OhiDrop = function(iAliasTo) {
+      sWs.send(JSON.stringify({op:'ohi_drop', ohi:{alias:iAliasTo}}))
+   };
+
+   mnm.PingSave = function(iObj) { // with alias, to, text, gid
+      sWs.send(JSON.stringify({op:'ping_save', ping:iObj}))
+   };
+   mnm.PingDiscard = function(iObj) { // with to, gid
+      sWs.send(JSON.stringify({op:'ping_discard', ping:iObj}))
+   };
+   mnm.PingSend = function(iObj) { // with to, gid
+      sWs.send(JSON.stringify({op:'ping_send', ping:iObj}))
+   };
+   mnm.InviteAccept = function(iGid) {
+      sWs.send(JSON.stringify({op:'accept_send', accept:{gid:iGid}}))
+   };
+
+   mnm.History = function(i) {
+      sWs.send(JSON.stringify({op:'history', navigate:{history:i}}))
+   };
+
+   mnm.ThreadNew = function(iObj) { // with (data), alias, (cc)
+      iObj.id = '', iObj.new = true;
+      sWs.send(JSON.stringify({op:'thread_save', thread:iObj}))
+   };
+   mnm.ThreadRecv = function() {
+      sWs.send(JSON.stringify({op:'thread_recvtest', thread:{}}))
+   };
+   mnm.ThreadGo = function(iId) {
+      sWs.send(JSON.stringify({op:'thread_set', thread:{id:iId}}))
+   };
+   mnm.ThreadOpen = function(iId) {
+      _xhr('mn', iId);
+   };
+   mnm.ThreadClose = function(iId) {
+      sWs.send(JSON.stringify({op:'thread_close', thread:{id:iId}}))
+   };
+   mnm.ThreadSave = function(iObj) { // with (id), alias, (cc), (data), (attach), (form_fill)
+      delete iObj.new // just in case
+      sWs.send(JSON.stringify({op:'thread_save', thread:iObj}))
+   };
+   mnm.ThreadSend = function(iId) {
+      sWs.send(JSON.stringify({op:'thread_send', thread:{id:iId}}))
+   };
+   mnm.ThreadDiscard = function(iId) {
+      sWs.send(JSON.stringify({op:'thread_discard', thread:{id:iId}}))
+   };
+
+   mnm.TabAdd = function(iObj) { // with type, term
+      sWs.send(JSON.stringify({op:'tab_add', tab:iObj}))
+   };
+   mnm.TabSelect = function(iObj) { // with type, posfor, pos
+      sWs.send(JSON.stringify({op:'tab_select', tab:iObj}))
+   };
+   mnm.TabPin = function(iType) {
+      sWs.send(JSON.stringify({op:'tab_pin', tab:{type:iType}}))
+   };
+   mnm.TabDrop = function(iType) {
+      sWs.send(JSON.stringify({op:'tab_drop', tab:{type:iType}}))
+   };
+
+   mnm.Connect = function() {
+      var aSvc = window.location.pathname.split('/')[1];
+      sWs = new WebSocket('ws://'+window.location.host+'/s/'+aSvc);
+      sWs.onopen = function() {
+         sWs.send(JSON.stringify({op:'open'}));
+      };
+      sWs.onmessage = function(iEvent) {
+         mnm.Log(iEvent.data);
+         var aObj = JSON.parse(iEvent.data);
+         if (!(aObj instanceof Array))
+            return;
+         for (var a=0; a < aObj.length; ++a) {
+            if (aObj[a] === 'ml')
+               mnm.ThreadChange();
+            if (aObj[a] === 'mn' || aObj[a] === 'an' || aObj[a] === 'fn')
+               _xhr(aObj[a], aObj[++a]);
+            else
+               _xhr(aObj[a]);
+         }
+      };
+      sWs.onclose = function(iEvent) { mnm.Log('closed') };
+      sWs.onerror = function(iEvent) { mnm.Log('ws error: ' + iEvent.data) };
+   };
+
+   function _xhr(i, iP) {
+      var aXhr = new XMLHttpRequest();
+      aXhr.onload = function() {
+         if (i !== 'mo' && i !== 'mn') {
+            mnm.Render(i, aXhr.responseText);
+            return;
+         }
+         var aMap = {};
+         for (var a=0; a < aXhr.responseText.length; ) {
+            var aHeadLen = parseInt(aXhr.responseText.substr(a, 4), 16);
+            var aHead = JSON.parse(aXhr.responseText.substr(a+4, aHeadLen));
+            aHead.msg_data = aXhr.responseText.substr(a+4+aHeadLen+1, aHead.Len);
+            if (i === 'mn') {
+               mnm.Render(i, aXhr.responseText, aHead);
+               return;
+            }
+            aMap[aHead.Id] = aHead;
+            a += 4 + aHeadLen + aHead.Len + 2;
+            if (aHead.From === 'self' && aHead.SubHead.Attach) {
+               var aAtc = aHead.SubHead.Attach;
+               for (var aA=0; aA < aAtc.length; ++aA)
+                  if (aAtc[aA].Name.charAt(0) === 'r')
+                     a += aAtc[aA].Size;
+            }
+         }
+         mnm.Render(i, aXhr.responseText, aMap);
+      };
+      aXhr.open('GET', i.charAt(0) === '/' ? i+'/'+(iP || '') : '?'+i+(iP ? '='+iP : ''));
+      aXhr.send();
+   }
+
+}).call(this);
+
