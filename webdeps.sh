2f7d74cbc4c158c21cfd5955d3f43952c4a8295c
networkimprov <github@networkimprov.net>
2018-03-05 23:52:50 -0800

    webdeps.sh: Add markdown-it

--- webdeps.sh
+++ webdeps.sh
@@ -4,6 +4,7 @@ set -e
 
 CDN_UIKIT=https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40
 CDN_VUE=https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.13
+CDN_MDI=https://cdnjs.cloudflare.com/ajax/libs/markdown-it/8.4.1
 
 cd web
 
@@ -11,4 +12,5 @@ curl --silent --show-error --location \
      -o uikit-30.min.css      "$CDN_UIKIT/css/uikit.min.css" \
      -o uikit-30.min.js       "$CDN_UIKIT/js/uikit.min.js" \
      -o uikit-icons-30.min.js "$CDN_UIKIT/js/uikit-icons.min.js" \
-     -o vue-25.js             "$CDN_VUE/vue.js"
+     -o vue-25.js             "$CDN_VUE/vue.js" \
+     -o markdown-it-84.js     "$CDN_MDI/markdown-it.js"
