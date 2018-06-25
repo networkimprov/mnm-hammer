5dd100028d216dc346b5f049d266b52540a632ed
networkimprov <github@networkimprov.net>
2018-06-25 06:45:40 -0700

    webdeps: Add Luxon datetime library

--- webdeps.sh
+++ webdeps.sh
@@ -6,6 +6,7 @@ CDN_UIKIT=https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40
 CDN_VUE=https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.13
 CDN_MDI=https://cdnjs.cloudflare.com/ajax/libs/markdown-it/8.4.1
 CDN_VFG=https://unpkg.com/vue-form-generator@2.2.0
+CDN_LUX=https://unpkg.com/luxon@1.3.0
 
 cd web
 
@@ -15,5 +16,6 @@ curl --silent --show-error --location \
      -o uikit-icons-30.min.js "$CDN_UIKIT/js/uikit-icons.min.js" \
      -o vue-25.js             "$CDN_VUE/vue.js" \
      -o markdown-it-84.js     "$CDN_MDI/markdown-it.js" \
+     -o luxon-13.js           "$CDN_LUX/build/global/luxon.js" \
      -o vue-formgen-22.js     "$CDN_VFG/dist/vfg.js" \
      -o vue-formgen-22.css    "$CDN_VFG/dist/vfg.css"
