5ab9f023bb7f0ce68c22e2f4bb8d937b06b7a9dd
networkimprov <github@networkimprov.net>
2019-01-28 21:38:49 -0800

    test: Add polling report skips odd-numbered tries

--- test.go
+++ test.go
@@ -201,7 +201,7 @@ func _runTestClient(iTc *tTestClient, iWg *sync.WaitGroup) {
          }
       }
       aSum := (*int32)(nil); if aUpdt.Test != nil && aUpdt.Test.Poll > 0 { aSum = new(int32) }
-      for {
+      for aTryN := 1; true; aTryN++ {
          for a1 := 0; a1 < len(aOps); a1++ {
             aOp, aId := aOps[a1], ""
             if aOp == "_n" {
@@ -214,7 +214,7 @@ func _runTestClient(iTc *tTestClient, iWg *sync.WaitGroup) {
                aId = aOps[a1]
             }
             aCtx.wg.Add(1)
-            go _runTestService(&aCtx, aOp, aId, iTc.Orders[a].Result[aOp], aPrefix, aSum)
+            go _runTestService(&aCtx, aOp, aId, iTc.Orders[a].Result[aOp], aPrefix, aSum, aTryN)
          }
          aCtx.wg.Wait()
          if aSum == nil || *aSum == int32(len(aOps)) {
@@ -362,7 +362,7 @@ func _verifyNameList(iList []string, iExpect interface{}, iPrefix string) {
 }
 
 func _runTestService(iCtx *tTestContext, iOp, iId string, iExpect interface{},
-                    iPrefix string, iSum *int32) {
+                     iPrefix string, iSum *int32, iTryN int) {
    defer iCtx.wg.Done()
    var err error
    var aResult, aMis interface{}
@@ -431,9 +431,11 @@ func _runTestService(iCtx *tTestContext, iOp, iId string, iExpect interface{},
 
    aName, aMis = _hasExpected(iOp, iExpect, aResult)
    if aName != "" {
-      aWhat := "mismatch"; if iSum != nil { aWhat = "polling" }
-      fmt.Fprintf(os.Stderr, "%s %s\n  expect %v\n  got    %s %v\n",
-                             iPrefix, aWhat, iExpect, aName, aMis)
+      if iSum == nil || iTryN % 2 == 0 {
+         aWhat := "mismatch"; if iSum != nil { aWhat = "polling" }
+         fmt.Fprintf(os.Stderr, "%s %s\n  expect %v\n  got    %s %v\n",
+                                iPrefix, aWhat, iExpect, aName, aMis)
+      }
       return
    }
    if iSum != nil {
