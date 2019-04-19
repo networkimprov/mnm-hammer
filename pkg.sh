7b58020b3d644b4c25125331ed386885b482c710
networkimprov <github@networkimprov.net>
2019-04-19 13:23:23 -0700

    pkg: Add pkg.sh

---
+++ pkg.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#  Copyright 2019 Liam Breck
+#  Published at https://github.com/networkimprov/mnm-hammer
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+set -e
+
+bins=(
+   'GOOS=linux  GOARCH=amd64'
+   'GOOS=darwin GOARCH=amd64'
+)
+
+app="$(basename "$PWD")"
+files=("$app" LICENSE formspec test-in.json web/*.*)
+
+go build
+ver=($("./$app" --version))
+
+echo "packaging ${ver[@]} with:"
+ls -sd "${files[@]}"
+
+ln -s "$app" "../$app-${ver[-2]}"
+files=($(printf "$app-${ver[-2]}/%s " "${files[@]}"))
+
+for pf in "${bins[@]}"; do
+   export $pf
+   go build
+   echo -n "$GOOS-$GOARCH built "
+   dst="$app/mnm-app-$GOOS-$GOARCH-${ver[-2]}"
+   if [ $GOOS = none ]; then
+      (cd ..; zip -ryq "$dst.zip" "${files[@]}")
+   else
+      (cd ..; tar -czf "$dst.tgz" "${files[@]}")
+   fi
+   echo "& packaged"
+done
+
+rm "../$app-${ver[-2]}"
+
+GOOS='' GOARCH='' go build
+
